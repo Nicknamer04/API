dc6d0c0d3ad789b842f712b62c204368cc0c4a0c
Nicknamer04 <maeshfl2004@gmail.com>
2022-04-22 19:50:57 -0500

    listo el crud

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express')
+const bodyParser = require('body-parser')
+const api = require('./api')
 const port = process.env.PORT || 3000
 const app = express()
+
+app.use(bodyParser.json())
 
 app.listen(port,function(){
     console.log("server is listen at port: " + port)
@@ -8,3 +12,5 @@ app.listen(port,function(){
 app.get('/',function(req,res){
     res.send("Hola desde express")
 })
+
+app.use('/api', api)
